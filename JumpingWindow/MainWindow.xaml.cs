3a68963dee6e2d867860ba87e959020afc43da5b
MKamihira <MKamihira@intra.wiseman.co.jp>
2017-12-06 16:37:58 +0900

    プロジェクト ファイルを追加します。

---
+++ JumpingWindow/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using IronPython.Hosting;
+using Microsoft.Scripting.Hosting;
+namespace JumpingWindow
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var engineMain = Python.CreateEngine();
+            //ビルトインスコープに追加するとスクリプト側が複数ファイルで構成されていてもwindowが参照可能
+            ScriptScope builtin = engineMain.GetBuiltinModule();
+            builtin.SetVariable("window", this);
+            engineMain.ExecuteFile("jumping.py");
+        }
+    }
+}
